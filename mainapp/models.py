1093ba4e468412ae9b6a4b8550389181bd7b195d
Vasily <33507763+VasilyMagay@users.noreply.github.com>
2020-02-14 08:36:49 +0300

    Подготовка к развертыванию

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,16 +1,27 @@
 from django.db import models
+from authapp.models import SiteUser
 
 
-# class Site(models.Model):
-#     name = models.CharField(max_length=200, verbose_name='наименование')
-#     ref = models.CharField(max_length=1024, verbose_name='ссылка на ресурс')
-#
-#
-# class Topic(models.Model):
-#     name = models.CharField(max_length=200, verbose_name='наименование темы')
-#     keywords = models.CharField(max_length=1024, verbose_name='ключевые слова, фразы')
-#     is_off = models.BooleanField(verbose_name='тема отключена')
-#     site = models.ForeignKey(Site, verbose_name='сайт', on_delete=models.CASCADE)
-#
-#     choice = models.CharField(max_length=200)
-#     votes = models.IntegerField()
+class Site(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Name')
+    ref = models.CharField(max_length=1024, verbose_name='Reference')
+
+
+class Topic(models.Model):
+    user = models.ForeignKey(SiteUser, verbose_name='User', on_delete=models.CASCADE)
+    site = models.ForeignKey(Site, verbose_name='Site', on_delete=models.CASCADE)
+
+    name = models.CharField(max_length=200, verbose_name='Name')
+    keywords = models.CharField(max_length=1024, verbose_name='Keywords, phrases')
+    inactive = models.BooleanField(verbose_name='Inactive')
+    period_start = models.DateTimeField(blank=True, null=True, verbose_name='Active Period Start')
+    period_end = models.DateTimeField(blank=True, null=True, verbose_name='Active Period End')
+
+
+class News(models.Model):
+    user = models.ForeignKey(SiteUser, verbose_name='User', on_delete=models.CASCADE)
+    topic = models.ForeignKey(Topic, verbose_name='Topic', on_delete=models.CASCADE)
+
+    news_date = models.DateTimeField(blank=True, null=True, verbose_name='News date')
+    ref = models.CharField(max_length=1024, verbose_name='Reference')
+    brief_info = models.TextField(blank=True, verbose_name='Brief')
---
+++ news_parser/production_settings.py
@@ -0,0 +1,2 @@
+ALLOWED_HOSTS = ['magv.pythonanywhere.com', ]
+DOMAIN_NAME = 'https://magv.pythonanywhere.com:443'
